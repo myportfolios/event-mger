33154fde159b883f17fe5b15205df57b4e02e587
Adetola <lexybiade@gmail.com>
2022-06-13 10:50:56 -0400

    Install body-parser pkg for json and urlencoded request body

--- server/controllers/HomePageControllers.js
+++ server/controllers/HomePageControllers.js
@@ -7,7 +7,8 @@ const axios = require('axios');
  */
 exports.loginController = (req, res) => {
     // get username, email, and pasword from req body
-    console.log('body', req)
+    console.log('body', req?.body)
+    return
     // const {username, password} = req?.body;
     // if(!username && !password){
     //     res.send("Invalid Request").status(400)
--- server/index.js
+++ server/index.js
@@ -2,17 +2,30 @@
 //mongosh "mongodb+srv://cluster0.hkteh.mongodb.net/myFirstDatabase" --apiVersion 1 --username admin4EventsMger
 //import express
 const express = require('express');
+
 //import router
 const router = require('../server/routes/routes');
 //create instance
 const app = express();
 
+//import body-parser
+const bodyParser = require('body-parser')
+
+//create jsonParser for json request
+const jsonParser = bodyParser.json();
+
+//create urlencoder parser for urlencoder request from forms such as login/register
+const urlencoderParser = bodyParser.urlencoded();
+
 //connect the app to database when the app  starts
 require("./database/mongoose");
 
+app.use('/login', urlencoderParser)
+app.use('/register', urlencoderParser)
+app.use(jsonParser)
+
 //use router
 app.use(router);
-app.use(express.json());
 
 //start server
 const PORT = 8000
